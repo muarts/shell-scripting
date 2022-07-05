2583bd34aa506028494d33594333b642ca5a1513
murat.sokucu <murat.sokucu@trendyol.com>
2022-07-06 00:41:58 +0300

    first commit

---
+++ hello.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# this is a comment.
+
+# hello world example.
+echo "hello world!"
+
+# some of system variables
+echo Shell name is $BASH
+echo Shell version is $BASH_VERSION
+echo Home directory is $HOME
+echo Present working directory is $PWD
+
+# user-defined variables
+# note: variable names should not start with a number.
+name=Murat
+value=10
+echo My name is $name
+echo Value is $value
---
+++ read-user-input.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+echo "What is your name?"
+# input will be save into userName variable
+read userName
+echo "Hello $userName"
+
+# you can read multiple inputs.
+echo "Name three programming languages."
+read lang1 lang2 lang3
+echo "Your inputs are $lang1, $lang2, and $lang3."
+
+# read input from the same line
+read -p "Username: " username
+# silent input with -s
+read -sp "Password: " password
+echo
+echo "Your username is $username"
+echo "Your password is $password"
+
+
+# save multiple inputs into an array
+echo "Enter your favorite cities: "
+read -a cities
+echo "Your favorite cities are ${cities[0]}, ${cities[1]}, and ${cities[2]}"
+
+# if you don't write variable name after read command,
+# input will store into built-in variable $REPLY
+echo "What is your name?"
+read
+echo "Hello $REPLY"
